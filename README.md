77aebed9171d948d9e4d5842330c1023c42da98e
zls <32353492+zhenglisai@users.noreply.github.com>
2018-01-25 12:02:22 +0800

    保存工作

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # zabbixapi
 用python实现zabbixapi，方便在程序中直接调用和导入
+说明：
+一，程序中使用到的库文件
+    1，requests
---
+++ zabbixapi.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+import json
+import requests
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
